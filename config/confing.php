2210c99393d827ed742b22eeefb709ce7135b238
dev-jackson <59906403+dev-jackson@users.noreply.github.com>
2020-02-09 19:17:29 -0500

    Init

---
+++ config/confing.php
@@ -0,0 +1,12 @@
+<?php
+
+    //Emcabezado y Footer
+    define('HEADER','view/plantillas/header.php');
+    define('FOOTER','view/pantillas/footer.php');
+
+    //BDD
+    define('SERVIDOR','localhost');
+    define('PUERTO','3036');
+    define('NOMBRE','NO');
+    define('PASSWORD','');
+?>
